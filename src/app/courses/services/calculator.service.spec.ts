5f9e25f9a889af93375e2f8d4c0e6a22d26e1dfd
Your Name <you@example.com>
2019-04-04 15:50:33 +0200

    Angular Testing Course

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -1,3 +1,5 @@
+import {CalculatorService} from './calculator.service';
+import {LoggerService} from './logger.service';
 
 
 describe('CalculatorService', () => {
@@ -5,14 +7,22 @@ describe('CalculatorService', () => {
 
     it('should add two numbers', () => {
 
-        pending();
+        const calculator = new CalculatorService(new LoggerService());
+
+        const result = calculator.add(2, 2);
+
+        expect(result).toBe(4);
 
     });
 
 
     it('should subtract two numbers', () => {
 
-        fail();
+        const calculator = new CalculatorService(new LoggerService());
+
+        const result = calculator.subtract(2, 2);
+
+        expect(result).toBe(0, "unexpected subtraction result");
 
     });
 
