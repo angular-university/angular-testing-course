e718b5bdfc178108cf92b00b48cd776460e1f82b
Your Name <you@example.com>
2019-04-11 16:33:42 +0200

    Angular Testing Course

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -1,28 +1,52 @@
 import {CalculatorService} from './calculator.service';
 import {LoggerService} from './logger.service';
+import {TestBed} from '@angular/core/testing';
 
 
 describe('CalculatorService', () => {
 
+    let calculator: CalculatorService,
+        loggerSpy: any;
 
-    it('should add two numbers', () => {
+    beforeEach(()=> {
 
-        const calculator = new CalculatorService(new LoggerService());
+        console.log("Calling beforeEach");
+
+        loggerSpy = jasmine.createSpyObj('LoggerService', ["log"]);
+
+        TestBed.configureTestingModule({
+            providers: [
+                CalculatorService,
+                {provide: LoggerService, useValue: loggerSpy}
+            ]
+        });
+
+        calculator = TestBed.get(CalculatorService);
+
+    });
+
+    fit('should add two numbers', () => {
+
+        console.log("add test");
 
         const result = calculator.add(2, 2);
 
         expect(result).toBe(4);
+
+        expect(loggerSpy.log).toHaveBeenCalledTimes(1);
 
     });
 
 
     it('should subtract two numbers', () => {
 
-        const calculator = new CalculatorService(new LoggerService());
+        console.log("subtract test");
 
         const result = calculator.subtract(2, 2);
 
         expect(result).toBe(0, "unexpected subtraction result");
+
+        expect(loggerSpy.log).toHaveBeenCalledTimes(1);
 
     });
 
--- src/app/courses/services/calculator.service.ts
+++ src/app/courses/services/calculator.service.ts
@@ -19,7 +19,7 @@ export class CalculatorService {
 
   subtract(n1: number, n2:number) {
     this.logger.log("Subtraction operation called");
-    return n1 * n2;
+    return n1 - n2;
   }
 
 
