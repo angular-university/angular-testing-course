ca16c784911c4e21c71727b086eb90df7286a282
Your Name <you@example.com>
2019-06-07 11:22:41 +0200

    Angular Testing Course

--- cypress/integration/home.test.js
+++ cypress/integration/home.test.js
@@ -3,7 +3,7 @@
 
 describe('Home Page', () => {
 
-    it('should display a list of courses', () => {
+    beforeEach(() => {
 
         cy.fixture('courses.json').as("coursesJSON");
 
@@ -13,6 +13,10 @@ describe('Home Page', () => {
 
         cy.visit('/');
 
+    });
+
+    it('should display a list of courses', () => {
+
         cy.contains("All Courses");
 
         cy.wait('@courses');
@@ -21,4 +25,40 @@ describe('Home Page', () => {
 
     });
 
+    it('should display the advanced courses', () => {
+
+        cy.get('.mat-tab-label').should("have.length", 2);
+
+        cy.get('.mat-tab-label').last().click();
+
+        cy.get('.mat-tab-body-active .mat-card-title').its('length').should('be.gt', 1);
+
+        cy.get('.mat-tab-body-active .mat-card-title').first()
+            .should('contain', "Angular Security Course");
+
+    });
+
+
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
