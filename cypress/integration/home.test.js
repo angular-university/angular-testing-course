0ea664582ff0c736ceabf06d62088088e0cec574
Ivan Muliarchyk <ivan.muliarchyk5@gmail.com>
2020-01-19 21:55:48 +0300

    Add end to end tests

---
+++ cypress/integration/home.test.js
@@ -0,0 +1,28 @@
+describe('Home Page', () => {
+
+    beforeEach(() => {
+        cy.fixture('courses.json').as('coursesJSON');
+        cy.server();
+        cy.route('/api/courses', '@coursesJSON').as('courses');
+        cy.visit('/');
+    });
+
+    it('should display a list of courses', () => {
+
+        cy.contains('All Courses');
+
+        cy.wait('@courses');
+        cy.get('mat-card').should('have.length', 9);
+
+    });
+
+    it('should display the advanced courses', () => {
+
+        cy.get('.mat-tab-label').should('have.length', 2);
+        cy.get('.mat-tab-label').last().click();
+        cy.get('.mat-tab-body-active .mat-card-title').its('length').should('be.gt', 1);
+        cy.get('.mat-tab-body-active .mat-card-title').first().should('contain', 'Angular Security Course');
+
+    });
+
+});
