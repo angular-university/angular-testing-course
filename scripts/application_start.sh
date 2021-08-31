2e6c063c4660fcd689d83fa01d0f845de9fbb2ba
dev-ops-user <73810667+dev-ops-user@users.noreply.github.com>
2021-08-31 21:10:39 +0530

    Update application_start.sh

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -7,7 +7,7 @@ sudo chmod -R 777 /home/ec2-user/express-app
 cd /home/ec2-user/express-app
 
 #add npm and node to path
-npm install -g @angular/cli@12.2.2
+
 export NVM_DIR="$HOME/.nvm"	
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
