a0b335e75a5eb467d403f81d9aaf4cf78a175f11
dev-ops-user <73810667+dev-ops-user@users.noreply.github.com>
2021-08-31 21:11:42 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -5,6 +5,8 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
 nvm install node
 
+npm install -g @angular/cli@12.2.2
+
 #create our working directory if it doesnt exist
 DIR="/home/ec2-user/express-app"
 if [ -d "$DIR" ]; then
