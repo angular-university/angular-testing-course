8ebc8e937d83c8a8a69910f24d0143be3a73671a
max-in-bc <contact@maxgardiner.ca>
2021-11-20 14:29:08 -0800

    presentation component testing complete

---
+++ src/app/courses/courses-card-list/courses-card-list.component.spec.ts
@@ -0,0 +1,66 @@
+import { ComponentFixture, TestBed, waitForAsync} from '@angular/core/testing';
+import {CoursesCardListComponent} from './courses-card-list.component';
+import {CoursesModule} from '../courses.module';
+import {COURSES} from '../../../../server/db-data';
+import {DebugElement} from '@angular/core';
+import {By} from '@angular/platform-browser';
+import {sortCoursesBySeqNo} from '../home/sort-course-by-seq';
+import {Course} from '../model/course';
+import {setupCourses} from '../common/setup-test-data';
+
+
+
+
+describe('CoursesCardListComponent', () => {
+  let component: CoursesCardListComponent;
+  let fixture: ComponentFixture<CoursesCardListComponent>;
+  let el: DebugElement;
+
+  beforeEach(waitForAsync( () => {
+    TestBed.configureTestingModule({
+      imports: [
+        CoursesModule
+      ]
+    })
+      .compileComponents()
+      .then(() => {
+        fixture = TestBed.createComponent(CoursesCardListComponent);
+        component = fixture.componentInstance;
+        el = fixture.debugElement;
+      });
+  }));
+
+  it('should create the component', () => {
+    expect(component).toBeTruthy();
+  });
+
+
+  it('should display the course list', () => {
+    component.courses = setupCourses();
+    fixture.detectChanges();
+    const cards = el.queryAll(By.css('.course-card'));
+    expect(cards).toBeTruthy('Could not find cards');
+    expect(cards.length).toBe(12, 'Unexpected number of courses');
+  });
+
+
+  it('should display the first course', () => {
+    component.courses = setupCourses();
+    fixture.detectChanges();
+    const course = component.courses[0];
+    const card = el.query(By.css('.course-card:first-child'));
+    const title = card.query(By.css('mat-card-title'));
+    const image = card.query(By.css('img'));
+    expect(card).toBeTruthy('Could not find course card');
+    expect(title.nativeElement.textContent).toBe(course.titles.description);
+    expect(image.nativeElement.src).toBe(course.iconUrl);
+
+
+
+
+  });
+
+
+});
+
+
