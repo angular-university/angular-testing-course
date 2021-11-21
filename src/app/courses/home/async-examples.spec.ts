32db7d209f270f193879bf98adfe9526d20c697f
max-in-bc <contact@maxgardiner.ca>
2021-11-20 16:29:46 -0800

    async testing examples testing complete

--- src/app/courses/home/async-examples.spec.ts
+++ src/app/courses/home/async-examples.spec.ts
@@ -1,1 +1,80 @@
-describe
+import {fakeAsync, flush, flushMicrotasks, tick} from '@angular/core/testing';
+import {of} from 'rxjs';
+import {delay} from 'rxjs/operators';
+
+fdescribe('Async testing examples', () => {
+  it('Async test example with Jasmine done()', (done: DoneFn) => {
+    let test = false;
+
+    setTimeout(() => {
+      console.log('running assertions');
+      test = true;
+      expect(test).toBeTruthy();
+      done();
+    }, 1000);
+  });
+
+  it('Async test example setTimeout', fakeAsync(() => {
+    let test = false;
+
+    setTimeout(() => {
+    });
+    setTimeout(() => {
+      console.log('running assertions setTimeout');
+      test = true;
+    }, 1000);
+    flush();
+    // tick(500);
+    // tick(500);
+    expect(test).toBeTruthy();
+  }));
+
+  it('Async test example plan promise', fakeAsync(() => {
+    let test = false;
+    console.log('creating promise');
+
+    Promise.resolve().then(() => {
+      console.log('Promise resolve1 successfully');
+      return Promise.resolve();
+    }).then(() => {
+      test = true;
+      console.log('Promise resolve2successfully');
+    });
+    flushMicrotasks();
+    console.log('running assertions plan promise');
+    expect(test).toBeTruthy();
+  }));
+  it('Async test example plan promise + timeouts', fakeAsync(() => {
+    let counter = 0;
+
+    Promise.resolve().then(() => {
+      counter += 10;
+
+      setTimeout(() => {
+        counter += 1;
+      }, 1000);
+    });
+
+    expect(counter).toBe(0);
+    flushMicrotasks();
+    expect(counter).toBe(10);
+    tick(500);
+    expect(counter).toBe(10);
+    tick(500);
+    expect(counter).toBe(11);
+  }));
+
+  it('Async test example plan Observables', fakeAsync(() => {
+    let test = false;
+    console.log('Creating Observable');
+    const test$ = of(test).pipe(delay(1000));
+    test$.subscribe(() => {
+      test = true;
+    });
+
+    tick(1000);
+
+    console.log('running assertions');
+    expect(test).toBe(true);
+  }));
+});
